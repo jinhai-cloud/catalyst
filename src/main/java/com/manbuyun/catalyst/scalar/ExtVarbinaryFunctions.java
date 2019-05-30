d7ad425ad96c63d39185983e0f02ec8b84a51a0e
wangjinhai <wangjinhai@qutoutiao.net>
2019-05-30 20:27:31 +0800

    添加md5函数

---
+++ src/main/java/com/manbuyun/catalyst/scalar/ExtVarbinaryFunctions.java
@@ -0,0 +1,41 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.manbuyun.catalyst.scalar;
+
+import com.google.common.hash.Hashing;
+import io.airlift.slice.Slice;
+import io.airlift.slice.Slices;
+import io.prestosql.spi.function.Description;
+import io.prestosql.spi.function.LiteralParameters;
+import io.prestosql.spi.function.ScalarFunction;
+import io.prestosql.spi.function.SqlType;
+import io.prestosql.spi.type.StandardTypes;
+
+/**
+ * @author jinhai
+ * @date 2019/05/30
+ */
+public class ExtVarbinaryFunctions
+{
+    private ExtVarbinaryFunctions() {}
+
+    @Description("compute md5 hash")
+    @ScalarFunction
+    @LiteralParameters("x")
+    @SqlType(StandardTypes.VARCHAR)
+    public static Slice md5(@SqlType("varchar(x)") Slice slice)
+    {
+        return Slices.utf8Slice(Hashing.md5().hashBytes(slice.getBytes()).toString());
+    }
+}
